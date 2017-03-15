10c536ac74b57b25ea24939aab24ac4768b535e5
Dvessen83 <d.v.essen@gmail.com>
2017-03-15 12:10:50 +0100

    Add navbar specs

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,7 @@ family_friendly = Category.create(name: "Family friendly")
 john = User.create(email: "john@codaisseurup.com", password: "Koffie02")
 bob = User.create(email: "bob@codaisseurup.com", password: "Koffie02")
 harry = User.create(email: "harry@codaisseurup.com", password: "Koffie02")
+faker1 = User.create(email: Faker::Internet.email , password:  Faker::Internet.password )
 
 event1 = Event.create(name:"Coldplay", description:"concert album tour", location:"Wembley", price: 50, capacity: "string", includes_food: true, includes_drinks: true, starts_at:("03/10/2017 9:00"), ends_at:("03/10/2017 14:00"), active: true, user: john, categories: [concert, one_day])
 event2 = Event.create(name: "Streekbierfestival", description: "Bieren van lokale brouwerijen", location: "Utrecht", price:0, capacity: 20000, includes_food: false, includes_drinks: true, starts_at:("17/03/2017 13:00"), ends_at:("19/03/2017 23:00"), active: true, user: bob, categories: [festival])
