7904e0ce780f2228b5ece05a85049e0442e718bc
Dvessen83 <d.v.essen@gmail.com>
2017-03-16 14:58:42 +0100

    Added another photo to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,8 +18,9 @@ harry = User.create(email: "harry@codaisseurup.com", password: "Koffie02")
 photo1 = Photo.create(remote_image_url: "http://res.cloudinary.com/dxgom8aj1/image/upload/v1489670795/logo-nl_guc7s0.svg")
 photo2 = Photo.create(remote_image_url: "http://res.cloudinary.com/dxgom8aj1/image/upload/v1489670772/streekbierFestival_20Utrecht_20Banner_nhqroy.jpg")
 photo3 = Photo.create(remote_image_url: "http://res.cloudinary.com/dxgom8aj1/image/upload/v1489670726/Coldplay-feat_obgc58.jpg")
+photo4 = Photo.create(remote_image_url: "http://res.cloudinary.com/dxgom8aj1/image/upload/c_scale,h_376/v1489672484/gouden-tip-bg-1580x658_xlkwkm.jpg")
 
 #Events
 event1 = Event.create(name:"Coldplay", description:"concert album tour", location:"Wembley", price: 50, capacity: "string", includes_food: true, includes_drinks: true, starts_at:("03/10/2017 9:00"), ends_at:("03/10/2017 14:00"), active: true, user: john, categories: [concert, one_day], photos:[photo3])
 event2 = Event.create(name: "Streekbierfestival", description: "Bieren van lokale brouwerijen", location: "Utrecht", price:0, capacity: 20000, includes_food: false, includes_drinks: true, starts_at:("17/03/2017 13:00"), ends_at:("19/03/2017 23:00"), active: true, user: bob, categories: [festival], photos:[photo2])
-event3 = Event.create(name: "Pink Pop", description: "music festival", location: "Landgraaf", price: 200, capacity: 40000, includes_food: true, includes_drinks: true, starts_at:("16/06/2017 14:00"), ends_at:("19/06/2017 22:00"), active: true, user: harry, categories:[festival, family_friendly, concert], photos:[photo1])
+event3 = Event.create(name: "Pink Pop", description: "music festival", location: "Landgraaf", price: 200, capacity: 40000, includes_food: true, includes_drinks: true, starts_at:("16/06/2017 14:00"), ends_at:("19/06/2017 22:00"), active: true, user: harry, categories:[festival, family_friendly, concert], photos:[photo1, photo4])
