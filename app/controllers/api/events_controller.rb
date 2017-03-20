633a81a49f0de3e0314725e9fe2a023a8630113d
Dvessen83 <d.v.essen@gmail.com>
2017-03-20 16:43:27 +0100

    Add API events destropy and update

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,8 +1,66 @@
 class Api::EventsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def index
     render status: 200, json: {
       events: Event.all
     }.to_json
   end
 
+  def show
+    event = Event.find(params[:id])
+    render status: 200, json: {
+      event: event
+    }.to_json
+
   end
+
+  def create
+
+    event = Event.new(event_params)
+
+    if event.save
+      render status: 201, json: {
+        message: "Event created",
+        event: event
+      }.to_json
+    else
+      render status: 422, json: {
+        errors: event.errors
+      }.to_json
+    end
+  end
+
+  def destroy
+    event = Event.find(params[:id])
+    event.destroy
+
+    render status: 200, json: {
+      message: "Event destroyed"
+    }.to_json
+  end
+
+  def update
+    event = Event.find(params[:id])
+    if event.update(event_params)
+      render status: 202, json: {
+        message: "Event updated",
+        event: event
+      }.to_json
+    else
+      render status: 422, json: {
+        message: "The event could not be updated",
+        errors: event.errors
+      }.to_json
+
+    end
+
+
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :description, :location, :price, :capacity, :includes_food, :includes_drinks, :starts_at, :ends_at, :active, :user, category_ids: [])
+  end
+
+end
